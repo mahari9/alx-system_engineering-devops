09835c9561bc8cb2ac4491f5dd8c0c0552b423be
Mahari9 <merytseg77@gmail.com>
2024-04-19 07:19:45 -0700

    PUPPet Tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
+
+exec {'install python packages':
+  command => 'pip3 install flask==2.1.0 Werkzeug==3.0.2',
+  path    => ['/usr/bin/'],
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.8/dist-packages/flask/app.py',
+  }
