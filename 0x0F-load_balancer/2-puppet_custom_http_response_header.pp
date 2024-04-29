fd60f9dd4135384a4d545137b6f77de7c6500e7e
Mahari9 <merytseg77@gmail.com>
2024-04-29 11:43:21 -0700

    check

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,5 @@
-utomation: creates a custom HTTP header response with Puppet.
+# automate the task of creating a custom HTTP header response, but with Puppet
+
 exec { 'command':
   command  => 'apt-get -y update;
   apt-get -y install nginx;
