a5fbf1414189feab33144f3ccca4677672c06e29
Mahari9 <merytseg77@gmail.com>
2024-06-04 15:06:47 +0000

    Count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,21 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Queries the Reddit API for the subscriber count of a subreddit.
+
+    Args:
+      subreddit: The name of the subreddit to query.
+
+    Returns:
+      The number of subscribers (integer) or 0 if the subreddit is invalid.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    except requests.exceptions.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given words found in hot posts of a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:x86_64.api.advanced:v1.2.0 (by /u/bov_)"
     }
     params = {
         "after": after,
