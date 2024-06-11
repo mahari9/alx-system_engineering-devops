2a1c72ec64e5ccb28a4a7431cff4587e80f28173
Mahari9 <merytseg77@gmail.com>
2024-06-11 12:06:39 +0000

    Web stack debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# This manuscript enables the user holberton to login and open files without error
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
